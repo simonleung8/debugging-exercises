04760db198b66ebf8fa486e8ba6e17462774bd60
Simon Leung <leungs@us.ibm.com>
2016-10-12 22:30:17 +0800

    go-app needs memory

---
+++ need-memory/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func bigBytes() *[]byte {
+	s := make([]byte, 50000000)
+	return &s
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi, I am the answer to FDTR00801.")
+}
+
+func main() {
+	port := os.Getenv("PORT")
+	fmt.Fprintf(os.Stdout, "Obtained port from Env $PORT: "+port)
+
+	fmt.Fprintf(os.Stderr, "App requires minimum 256M to run, make sure enough memory is allocated")
+	for i := 0; i < 3; i++ {
+		s := bigBytes()
+		if s == nil {
+			fmt.Fprintf(os.Stderr, "App run out of memory, 256M is required")
+		}
+	}
+
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":"+port, nil)
+}
